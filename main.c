293d85f9d2420ad77117af92cf720f1c147f7506
anthony fusco <anthony.fusco42@gmail.com>
2017-05-06 14:49:28 +0200

    mes mains tape des lettres 2

--- main.c
+++ main.c
@@ -23,18 +23,20 @@ void mpi() {
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
 
-    // Print off a hello world message
-    /*printf("Hello world from processor %s, rank %d"
-           " out of %d processors\n",
-           processor_name, world_rank, world_size);*/
-
     int number;
     if(world_rank == 0) {
-        for(int i = 1; i < world_size; i++) {
+        /*for(int i = 1; i < world_size; i++) {
             printf("send %d\n", world_rank);
             number = i * 10;
             MPI_Send(&number, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-        }
+        }*/
+
+// Create a buffer that will hold a subset of the random numbers
+        // float *sub_rand_nums = malloc(sizeof(float) * elements_per_proc);
+
+// Scatter the random numbers to all processes
+        //MPI_Scatter(rand_nums, elements_per_proc, MPI_FLOAT, sub_rand_nums,
+        //            elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
     }
 
     number = -1;
@@ -93,7 +95,7 @@ long countElementOfFile(char* fileName) {
     return nbElem;
 }
 
-void readMatrixFromFile(Matrix* matrix, char* fileName) {
+long readMatrixFromFile(Matrix* matrix, char* fileName) {
     long nbElement = countElementOfFile(fileName);
     long lines = (long) sqrt(nbElement);
     long columns = (long) sqrt(nbElement);
@@ -109,6 +111,7 @@ void readMatrixFromFile(Matrix* matrix, char* fileName) {
         }
         fclose(file);
     }
+    return lines;
 }
 
 void printMatrix(Matrix* matrix) {
@@ -124,20 +127,87 @@ void printMatrix(Matrix* matrix) {
     }
 }
 
+void turnMatrix(Matrix* matrix, Matrix* turned) {
+    turned->columns = matrix->lines;
+    turned->lines = matrix->columns;
+    for(int i = 0; i < matrix->lines; i++){
+        for(int j = 0; j < matrix->columns; j++) {
+            set(turned, j, i, get(matrix, i, j));
+        }
+    }
+}
+
+void dispatchMatrix(long n){
+
+// Create a buffer that will hold a subset of the random numbers
+   // float *sub_rand_nums = malloc(sizeof(float) * elements_per_proc);
+
+// Scatter the random numbers to all processes
+    //MPI_Scatter(rand_nums, elements_per_proc, MPI_FLOAT, sub_rand_nums,
+    //            elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
+}
+
+void initMPI() {
+    MPI_Init(NULL, NULL);
+    //omp_set_num_threads(4);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+}
+
 int main(int argc, char** argv) {
-    //mpi();
+
+    MPI_Init(NULL, NULL);
+    //omp_set_num_threads(4);
+
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
     char* AFileName = argv[1];
-
     Matrix A;
-    readMatrixFromFile(&A, AFileName);
-    printMatrix(&A);
 
-    char* BFileName = argv[2];
 
-    Matrix B;
-    readMatrixFromFile(&B, BFileName);
+    if (world_rank == 0) {
+        long N = readMatrixFromFile(&A, AFileName);
+        //printMatrix(&A);
+
+        char *BFileName = argv[2];
+        Matrix B;
+        readMatrixFromFile(&B, BFileName);
+        //printf("\n");
+        //printMatrix(&B);
+        //printf("\n");
+        Matrix turned;
+        turnMatrix(&B, &turned);
+        //printMatrix(&turned);
+    }
+
+    // Create a buffer that will hold a subset of the numbers
+    int *sub_rand_nums = malloc(sizeof(int) * 20); //todo nbElemParLigne² / proc
+
+    int elements_per_proc = 20;
+    MPI_Scatter(A.tab, elements_per_proc, MPI_INT, sub_rand_nums,
+                elements_per_proc, MPI_INT, 0, MPI_COMM_WORLD);
+
+    Matrix received;
+    received.lines = 2;
+    received.columns = 10;
+    received.tab = sub_rand_nums;
     printf("\n");
-    printMatrix(&B);
+    printf("I am proc %d\n", world_rank);
+    printMatrix(&received);
+    printf("\n");
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
 }
 
 
